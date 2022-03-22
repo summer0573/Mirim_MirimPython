a976959ed34688721ff6d03949dd784c42aedbd8
정현진 <s2113@e-mirim.hs.kr>
2022-03-22 10:14:09 +0900

    변수 선언과 초기화, 여러가지 출력 방법

--- 20220322_helloworld.py
+++ 20220322_helloworld.py
@@ -1,3 +1,7 @@
 print("Hello, World!") #출력은 print 주석은 #
-print('Hello, 임성훈 선생님!')
+name='양다연'
+print('Hello, '+name+'!') #자바 형식
+print('Hello, %s!'%name) #C 형식
+print('Hello, {}!'.format(name)) #format 함수
+print(f'Hello, {name}!') #f-string 형식
 print("Stray Kids's favorite song is Winter Falls")
