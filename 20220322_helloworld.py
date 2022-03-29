dbc9e0872a0d8183a72f3e24c14dd3ff994f3ae8
정현진 <s2113@e-mirim.hs.kr>
2022-03-29 10:16:50 +0900

    변수 선언과 초기화, 여러가지 출력 방법

--- 20220322_helloworld.py
+++ 20220322_helloworld.py
@@ -5,3 +5,7 @@ print('Hello, %s!'%name) #C 형식
 print('Hello, {}!'.format(name)) #format 함수
 print(f'Hello, {name}!') #f-string 형식
 print("Stray Kids's favorite song is Winter Falls")
+
+student_number = 2111
+name = "정현진"
+print(f'학번: {student_number}, 이름: {name}') #학번: 2111 이름: 정현진
---
+++ 20220329_type.py
@@ -0,0 +1,22 @@
+#숫자
+student_number = 3419
+age = 18
+height = 160.2
+
+#문자
+name = '정현진'
+
+print(f'학번: {student_number}\n이름: {name}\n나이: {age}\n키: {height}')
+
+print(type(student_number)) #<class 'int'>
+print(type(name)) #<class 'str'>
+print(type(age)) #<class 'int'>
+print(type(height)) #c<lass 'float'>
+print(type(10.27)) #c<lass 'float'>
+print(type(1027)) #<class 'int'>
+print(type('1027')) #<class 'str'>
+print(10/27) #java: 0, python:0.37037037037037035
+print(27/10) #java: 2, python:2.7
+print(27//10) #python: 나눈 몫: 2
+print(27%10) #python: 나머지:
+print(type(27/10))
