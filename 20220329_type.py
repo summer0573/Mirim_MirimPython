10d2e5ba30be3cbb8e792d0627b288d067579827
정현진 <s2113@e-mirim.hs.kr>
2022-04-12 09:21:13 +0900

    10진수 -> 2, 8, 16진수, 지수표현, 복소수 표현

--- 20220329_type.py
+++ 20220329_type.py
@@ -51,4 +51,21 @@ print(0xFF) #16진수 FF => 255
 #10진수 -> 2진수
 print(bin(10)) #0b1010
 print(bin(9)) #0b1001
+print(oct(10)) #0o12
+print(hex(10)) #0xa
 
+#지수 표현
+print(f'지구의 나이 : {4.543e9}살')
+print(f'원자의 크기 : 10^-10m') #문자니까 값 계산 X float
+print(f'원자의 크기 : {1e-10}') #float
+
+#복소수 j
+print(9+1J-4-5j) #(5-4j)
+ys = 9+1j
+hj=7-3j
+print(ys+hj) #(16-2j)
+print(ys.real) #9.0 실수만 출력하기
+print(hj.imag)  #-3.0 허수만 출력하기
+print(hj.conjugate()) #켤레복소수
+print(hj * hj.conjugate()) #(58+0j) = 49 + (-3j x 3j) = 49 + 9 = (59 + 0j)
+print(type(ys)) #<class 'complex'> complex 종합
