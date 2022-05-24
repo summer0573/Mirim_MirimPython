406a9a363ab263e4edc1a060dcd1c505d29c6815
정현진 <s2113@e-mirim.hs.kr>
2022-05-24 10:18:08 +0900

    for문

--- 20220524_for문.py
+++ 20220524_for문.py
@@ -29,13 +29,13 @@
 #     print(val)
 # for key, val in pl.items(): #문자와 숫자 각각 저장
 #     print(key, val)
-
+#
 # #num_list 문제
 # for num_list in [-5, 127, -13, 9, -21, 100]:
 #     if num_list > 0:
 #         print(num_list)
-
-#짝수, 홀수
+#
+# 짝수, 홀수
 # num_list = [13, 8, 7, 55, 100, 2, 12, 10, 17]
 # for num in num_list:
 #     if num % 2 == 0:
@@ -51,17 +51,17 @@
 # #자리수
 # for num in num_list:
 #     print('{}은 {}자리수이다.'.format(num,len(str(num))))
-
+#
 # skz = {"한":90, "창빈":30, "아이엔":95, "승민": 50, "현진":100, "방찬":10, "리노":60, "용복":100}
 # for name, score in skz.items():
 #     if score >= 60 :
 #         print('{}은/는 합격입니다.'.format(name))
 #     else :
 #         print('{}은/는 불합격입니다.'.format(name))
-
-#range()함수
-#range(시작값, 종료값, 단계)
-#range(기본값 0, 종료값, 기본값 1)
+#
+# range()함수
+# range(시작값, 종료값, 단계)
+# range(기본값 0, 종료값, 기본값 1)
 # print(list(range(0, 10, -1)))
 # print(list(range(10, 0, -1))) #단계값이 음수면 역순
 # print(list(range(0, 10, 5)))
@@ -83,22 +83,32 @@
 #
 # for i, member in enumerate(nctdream):
 #     print(i+1, member)
-
-#1번 문제
-ssuumm = 0
-for num in range(0, 200, 3):
-    ssuumm += num
-
-print(f'{ssuumm}')
-
-#2번 문제
-ssuumm = 0
-for num in range (500 , 1000, 5):
-    ssuumm+=num
-
-print(f'{ssuumm}')
-
-l = [1,2,3,4,5]
-print(sum(l))
-print(max(l))
-print(min(l))
+#
+# 1번 문제
+# ssuumm = 0
+# for num in range(0, 200, 3):
+#     ssuumm += num
+#
+# print(f'{ssuumm}')
+#
+# #2번 문제
+# ssuumm = 0
+# for num in range (500 , 1000, 5):
+#     ssuumm+=num
+#
+# print(f'{ssuumm}')
+#
+# l = [1,2,3,4,5]
+# print(sum(l))
+# print(max(l))
+# print(min(l))
+#
+# for i in range(1,10): #2단만 출력
+#     print('2 X {} = {}'.format(i, (2 * i)))
+#
+# for i in range(2, 10): #2~9단 모두다 출력
+#     for j in range(1,10):
+#         print('{} X {} = {}'.format(i, j, (i*j)))
+#
+#     print('=======================================')
+#
